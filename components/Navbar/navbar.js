4b962fc6f73f2244b26d193bc10b9a5c7286d4bc
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-18 00:18:56 +0200

    We saved the page

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -4,8 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// Only here for deployment
+const theme = createMuiTheme({
+  palette: {},
+});
+
+export default theme;
