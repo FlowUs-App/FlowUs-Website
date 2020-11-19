87078bd244682a2beef87b4331f1244c0bd23a0c
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-19 18:28:54 +0100

    Removed firebase for now again

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { firebase } from "../res/ApiKey";
+import { FirebaseContext } from "../context/firebase";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-      </Head>
-      <Component {...pageProps} />
+      <FirebaseContext.Provider value={firebase}>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </FirebaseContext.Provider>
     </>
   );
 }
---
+++ pages/dashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function dashboard() {
+  return <div>logged in</div>;
+}
+
+export default dashboard;
