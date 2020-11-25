5500c39ebd08894b303833ec7c35b3042cb9a4f3
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-25 13:31:44 +0100

    added meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>LiFit App Social Media Fitness Network Landing Page</title>
+        <meta
+          name="description"
+          content="The official landing page of the LiFit Social Media Fitness Network App. Catch up with our latest news or login into your dashboard."
+        />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1, maximum-scale=5"
         />
       </Head>
       <Component {...pageProps} />
