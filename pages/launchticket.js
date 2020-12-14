9ba1cc7a1d39598a8f3efb8020be43b7d1a4dc4d
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-14 09:41:52 +0100

    Fixxed fontFamily of Logo

--- pages/launchticket.js
+++ pages/launchticket.js
@@ -1,4 +1,4 @@
-import { Grid } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -8,6 +8,16 @@ const useStyles = makeStyles((theme) => ({
   },
   grid: {
     marginTop: "1rem",
+  },
+  mainButton: {
+    backgroundColor: "#56377E",
+    position: "center",
+    fontFamily:
+      "Lato, -apple-system, Helvetica Neue, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, sans-serif",
+    color: "#FFF",
+    width: "7rem",
+    marginRight: "3rem",
+    borderRadius: "30px",
   },
 }));
 
@@ -22,7 +32,12 @@ function launchticket() {
         className={classes.grid}
       >
         <Grid item className={classes.width}>
-          <Grid container justify="space-between">
+          <Grid
+            container
+            justify="space-between"
+            alignContent="center"
+            alignItems="center"
+          >
             <Grid item>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -81,7 +96,7 @@ function launchticket() {
                     transform="translate(119 85)"
                     fill="#56377e"
                     fontSize="30"
-                    fontFamily="OpenSans-Bold, Open Sans"
+                    fontFamily="OpenSans-Bold, Open Sans, Arial"
                     fontWeight="700"
                   >
                     <tspan x="0" y="0">
@@ -92,72 +107,9 @@ function launchticket() {
               </svg>
             </Grid>
             <Grid item>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="193"
-                height="41"
-                viewBox="0 0 193 41"
-              >
-                <rect
-                  id="Rechteck_1062"
-                  data-name="Rechteck 1062"
-                  width="41"
-                  height="8"
-                  transform="translate(0 41) rotate(-90)"
-                  fill="#56377e"
-                />
-                <g
-                  id="Gruppe_70166"
-                  data-name="Gruppe 70166"
-                  transform="translate(-58 -53)"
-                >
-                  <rect
-                    id="Rechteck_1060"
-                    data-name="Rechteck 1060"
-                    width="29"
-                    height="7"
-                    transform="translate(61 87)"
-                    fill="#56377e"
-                  />
-                  <rect
-                    id="Rechteck_1061"
-                    data-name="Rechteck 1061"
-                    width="21"
-                    height="7"
-                    transform="translate(69 70)"
-                    fill="#56377e"
-                  />
-                  <rect
-                    id="Rechteck_1063"
-                    data-name="Rechteck 1063"
-                    width="28"
-                    height="7"
-                    transform="translate(69 53)"
-                    fill="#56377e"
-                  />
-                  <rect
-                    id="Rechteck_1064"
-                    data-name="Rechteck 1064"
-                    width="41"
-                    height="8"
-                    transform="translate(93 94) rotate(-90)"
-                    fill="#56377e"
-                  />
-                  <text
-                    id="LiFit_App"
-                    data-name="LiFit App"
-                    transform="translate(119 85)"
-                    fill="#56377e"
-                    font-size="30"
-                    font-family="OpenSans-Bold, Open Sans"
-                    font-weight="700"
-                  >
-                    <tspan x="0" y="0">
-                      LiFit App
-                    </tspan>
-                  </text>
-                </g>
-              </svg>
+              <Button size="medium" className={classes.mainButton}>
+                Main
+              </Button>
             </Grid>
           </Grid>
         </Grid>
