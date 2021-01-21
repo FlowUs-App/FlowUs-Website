3dea5b15a7b7cc90fe8358cb13d5d25cbc6e3f5f
lucas-goldner <lucas.goldner@googlemail.com>
2021-01-21 13:19:55 +0100

    Another fix commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0,  viewport-fit=cover"
-        />
-      </Head>
+      <Head></Head>
       <Component {...pageProps} />
     </>
   );
