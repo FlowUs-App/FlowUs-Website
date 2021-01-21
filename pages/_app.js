a9ef631367807d9254b0d201e61342a478f8eea1
lucas-goldner <lucas.goldner@googlemail.com>
2021-01-21 04:41:32 +0100

    This should do it

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import "../styles/globals.scss";
 import "../index.scss";
 import "swiper/swiper.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0,  viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
