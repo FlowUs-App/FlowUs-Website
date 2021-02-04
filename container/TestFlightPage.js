b23876b27abcb24be85f549081d871ffe7d6d62a
lucas-goldner <lucas.goldner@googlemail.com>
2021-02-04 09:06:29 +0100

    feat: Add confetti + testflight link

--- container/TestFlightPage.js
+++ container/TestFlightPage.js
@@ -4,8 +4,11 @@ import ContactUs from "../components/ContactUs";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import DefaultLayout from "../layout/DefaultLayout";
+import Confetti from "react-confetti";
+import useWindowSize from "../utils/useWindow";
 
 function TestFlightPage() {
+  const size = useWindowSize();
   return (
     <>
       <Fragment>
@@ -16,11 +19,23 @@ function TestFlightPage() {
           className="template-color-2 template-font-3 bg_color--4"
           revealFooter={true}
         >
+          <Confetti
+            width={size.width}
+            height={size.height}
+            colors={["#00bcd4", "#000ed4"]}
+            numberOfPieces={50}
+            recycle={false}
+          />
           <Header />
           <div className="eatPage centerThis">
-            <h1 className="section-title text-center textColorWhite centerMe">
-              Coming soon
-            </h1>
+            <a
+              href="https://testflight.apple.com/join/XA8bLv0r"
+              className="centerThis testText"
+            >
+              <h1 className="section-title text-center textColorWhite centerMe">
+                Start Testing
+              </h1>
+            </a>
           </div>
           <ContactUs />
         </DefaultLayout>
